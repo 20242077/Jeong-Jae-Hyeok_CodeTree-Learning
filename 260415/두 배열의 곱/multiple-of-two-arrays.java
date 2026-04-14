fd10e9b16439a02b659072ef4cd4f2e16e6e161e
20242077 <20242077@edu.hanbat.ac.kr>
2026-04-15 03:25:26 +0900

    Title: 두 배열의 곱, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260415/두 배열의 곱/multiple-of-two-arrays.java
+++ 260415/두 배열의 곱/multiple-of-two-arrays.java
@@ -2,27 +2,33 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Please write your code here.
-    Scanner sc = new Scanner(System.in);
-    int rows = 3, cols = 3;
-    int [][] arr_1 = new int[rows][cols], arr_2 = new int[rows][cols];
+        Scanner sc = new Scanner(System.in);
+        int rows = 3, cols = 3;
+        int[][] arr_1 = new int[rows][cols];
+        int[][] arr_2 = new int[rows][cols];
 
-    for(int n = 0; n < 2; n++){
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                if(n == 0){
-                    arr_1[i][j] = sc.nextInt();
-                }else{
-                    arr_2[i][j] = sc.nextInt();
-                }
+        // 첫 번째 행렬 입력
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                arr_1[i][j] = sc.nextInt();
             }
         }
-    }
-    for(int i = 0; i < rows; i++){
-        for(int j = 0; j < cols; j++){
-            System.out.print(arr_1[i][j]*arr_2[i][j]+" ");
+
+        // 두 번째 행렬 입력
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                arr_2[i][j] = sc.nextInt();
+            }
         }
-        System.out.println();
-    }
+
+        // 결과 출력 (곱셈과 출력을 동시에 수행)
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print((arr_1[i][j] * arr_2[i][j]) + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close(); // 자원 해제
     }
 }
