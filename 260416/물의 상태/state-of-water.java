fc8692f5f71fed0c8cbf5e9567c8bec04408a6b3
20242077 <20242077@edu.hanbat.ac.kr>
2026-04-16 01:58:19 +0900

    Title: 물의 상태, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260416/물의 상태/state-of-water.java
+++ 260416/물의 상태/state-of-water.java
@@ -1,19 +1,21 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Please write your code here.
-    Scanner sc = new Scanner(System.in);
-    while(true){
-        int n = sc.nextInt();
-        if(n > 200 || n < -200){
-            System.out.println("input -200 <= n <= 200");
-        }else if(n < 0){
-            System.out.println("ice");
-            break;
-        }else{
-            System.out.println("water");
-            break;
+        Scanner sc = new Scanner(System.in);
+        
+        // 안내 메시지나 무한 루프 없이 한 번만 입력받습니다.
+        if (sc.hasNextInt()) {
+            int n = sc.nextInt();
+            
+            // 문제에서 요구하는 출력값만 딱 출력합니다.
+            if (n < 0) {
+                System.out.println("ice");
+            } else {
+                System.out.println("water");
+            }
         }
+        
+        sc.close();
     }
-}
 }
