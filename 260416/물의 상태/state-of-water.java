0bc0671839cea85e4558053d45783eb966e4b022
20242077 <20242077@edu.hanbat.ac.kr>
2026-04-16 02:02:43 +0900

    Title: 물의 상태, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260416/물의 상태/state-of-water.java
+++ 260416/물의 상태/state-of-water.java
@@ -1,23 +1,27 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Please write your code here.
-    Scanner sc = new Scanner(System.in);
-    while(true){
-        int n = sc.nextInt();
-        if(n > 200 || n < -200){
-            System.out.println("input -200 <= n <= 200");
-            continue;
-        }if(n < 0){
-            System.out.println("ice");
-            break;
-        }else if(n>0 && n<100){
-            System.out.println("water");
-            break;
-        }else{
-            System.out.println("vapor");
-            break;
+        Scanner sc = new Scanner(System.in);
+
+        // 입력이 있을 때만 실행하도록 합니다.
+        if (sc.hasNextInt()) {
+            int n = sc.nextInt();
+
+            // 1. 0도 미만인 경우
+            if (n < 0) {
+                System.out.println("ice");
+            } 
+            // 2. 100도 이상인 경우 (새로 추가된 조건)
+            else if (n >= 100) {
+                System.out.println("vapor");
+            } 
+            // 3. 그 외의 경우 (0도 이상 100도 미만)
+            else {
+                System.out.println("water");
+            }
         }
+        
+        sc.close();
     }
-}
 }
