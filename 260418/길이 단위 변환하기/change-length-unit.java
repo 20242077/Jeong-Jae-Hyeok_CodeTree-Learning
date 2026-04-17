2429c2fd64593be902bca0015212a139f03bbae6
20242077 <20242077@edu.hanbat.ac.kr>
2026-04-18 01:56:20 +0900

    Title: 길이 단위 변환하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260418/길이 단위 변환하기/change-length-unit.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        double ft = 30.48, mi = 160934, a = 9.2, b = 1.3;
+        System.out.printf("%.1fft = %.1fcm\n",a,ft*a);
+        System.out.printf("%.1fmi = %.1fcm",b,mi*b);
+    }
+}
