87218e9528f5e8d1ac23a9b49c556910cd4063be
20242077 <20242077@edu.hanbat.ac.kr>
2026-04-18 03:44:47 +0900

    Title: 특정 조건 세 정수 비교, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260418/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
+++ 260418/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
@@ -7,12 +7,9 @@ public class Main {
         int b = sc.nextInt();
         int c = sc.nextInt();
 
-        if((a-b>0)||(a-c>0)){//이때 둘 중 하나라도 음수가 나오면 0출력 (a - b < 0) || (a - c < 0) 사용 --> 음수 판별 끝
-
-            System.out.print(0+" ");
-        }else if(((a-b==0)&&(a-c<0))||((a-c==0)&&(a-b0))){
+        if((a<=b)&&(b<=c)){
             System.out.print(1+" ");
-        }else if((a==b)&&(b==c)){
+        }else{
             System.out.print(0+" ");
         }
 
