16bb7f75268d2def020f9f14ec2194b76dfc71a2
20242077 <20242077@edu.hanbat.ac.kr>
2026-04-18 01:39:17 +0900

    Title: 정수 선언하고 뺄셈, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260418/정수 선언하고 뺄셈/define-numbers-and-substract.java
+++ 260418/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // Please write your code here.
-        System.out.print("97 - 13 = 84");
+        int a = 97, b = 13;
+        System.out.printf("%d - %d = %d",a,b,a-b);
     }
 }
