0e2700dae7f4f192c18da0e23a1a6095849739eb
20242077 <20242077@edu.hanbat.ac.kr>
2026-04-18 03:45:24 +0900

    Title: 특정 조건 세 정수 비교, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260418/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
+++ 260418/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
@@ -1,21 +1,24 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Please write your code here.
         Scanner sc = new Scanner(System.in);
+        
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
 
-        if((a<=b)&&(b<=c)){
-            System.out.print(1+" ");
-        }else{
-            System.out.print(0+" ");
+        // 1. 첫 번째 조건: a가 최솟값인가?
+        if (a <= b && a <= c) {
+            System.out.print(1 + " ");
+        } else {
+            System.out.print(0 + " ");
         }
 
-        if((a==b)&&(b==c)){
+        // 2. 두 번째 조건: 세 수가 모두 같은가?
+        if (a == b && b == c) {
             System.out.print(1);
-        }else{
+        } else {
             System.out.print(0);
         }
     }
