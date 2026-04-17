adef2a9af520207b5b0cfe43d968905fa3d4c1da
20242077 <20242077@edu.hanbat.ac.kr>
2026-04-18 02:44:39 +0900

    Title: 데이터 교환, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260418/데이터 교환/exchange-data.java
+++ 260418/데이터 교환/exchange-data.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         // Please write your code here.
-        int a = 5, b = 6, c = 7, temp1, temp2;
-        temp1 = b;
-        b = a;
-        temp2 = c;
-        c = temp1;
-        a = temp2;
+        int a = 5, b = 6, c = 7, temp;
+        temp = a;
+
+        a = c;
+        c = b;
+        b = temp;
         System.out.printf("%d\n%d\n%d",a,b,c);
     }
 }
