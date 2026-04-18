3e43d83800fc6b8d346d754c135cf4a897d3363d
20242077 <20242077@edu.hanbat.ac.kr>
2026-04-19 02:55:35 +0900

    Title: 1시간 뒤 시간 출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260419/1시간 뒤 시간 출력/print-one-hour-later.java
+++ 260419/1시간 뒤 시간 출력/print-one-hour-later.java
@@ -5,4 +5,7 @@ public class Main {
     Scanner sc = new Scanner(System.in);
     String t = sc.next();
     String[] strArr = t.split(":");
-    System.out.println(strArr[0]+strArr[1]);
+    int h = Integer.parseInt(strArr[0]), m = Integer.parseInt(strArr[1]);
+    System.out.printf("%d:%d",h+1,m);
+    }
+}
