eb530a2f97048aa8613c8ba272074d49ecacdaa3
20242077 <20242077@edu.hanbat.ac.kr>
2026-04-19 02:47:15 +0900

    Title: 1시간 뒤 시간 출력, Time: 0ms, Memory: 0MB, Status: Compile Error - Codetree

---
+++ 260419/1시간 뒤 시간 출력/print-one-hour-later.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+    Scanner sc = new Scanner(System.in);
+    String t = sc.next();
+    String[] strArr = t.split(":");
+    System.out.println(strArr[0]+strArr[1]);
