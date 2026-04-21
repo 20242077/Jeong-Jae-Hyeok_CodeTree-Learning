19538fce8dcb34ad8f9d9189e8427361756a1c60
20242077 <20242077@edu.hanbat.ac.kr>
2026-04-22 02:09:51 +0900

    Title: 살 수 있는 물건 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260422/살 수 있는 물건 2/things-able-to-buy-2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+    Scanner sc = new Scanner(System.in);
+    int money = sc.nextInt();
+    if(money >= 3000){
+        System.out.print("book");
+    }else if(money >= 1000){
+        System.out.print("mask");
+    }else if(money >= 500){
+        System.out.print("pen");
+    }else{
+        System.out.print("no");
+    }
+    sc.close();
+    }
+}
