c1dd01b5d49280d24854fb67142cbf862f190865
20242077 <20242077@edu.hanbat.ac.kr>
2026-04-27 12:56:45 +0900

    Title: 수의 계절은, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260427/수의 계절은/season-of-num.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+    Scanner sc = new Scanner(System.in);
+    int m = sc.nextInt();
+    String s = "";
+    if((m>=3)&&(m<=5)){
+        s = "Spring";
+    }else if((m>=6)&&(m<=8)){
+        s = "Summer";
+    }else if((m>=9)&&(m<=111)){
+        s = "Fall";
+    }else{
+        s = "Winter";
+    }
+    System.out.print(s);
+    sc.close();
+    }
+}
