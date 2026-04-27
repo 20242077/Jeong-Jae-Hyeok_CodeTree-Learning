8de137e6a308dba100aea8e5fcf51d0f80a9722a
20242077 <20242077@edu.hanbat.ac.kr>
2026-04-27 14:50:55 +0900

    Title: 두 사람, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260427/두 사람/two-person.java
+++ 260427/두 사람/two-person.java
@@ -5,9 +5,11 @@ public class Main {
     Scanner sc = new Scanner(System.in);
     int age1 = sc.nextInt();
     String gen1 = sc.next();
+
     int age2 = sc.nextInt();
     String gen2 = sc.next();
-    if(( (age1>=19) || (age2>=19) )&&( gen1.equals("M") || gen2.equals("M") )){
+
+    if((age1>=19)&&(gen1.equals("M"))||(age2>=19)&&(gen2.equals("M"))){
         System.out.print(1);
     }else{
         System.out.print(0);
