8f05ed0897ff09933e9ae92defe39d78b9336841
20242077 <20242077@edu.hanbat.ac.kr>
2026-04-27 14:36:59 +0900

    Title: 두 사람, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260427/두 사람/two-person.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+    Scanner sc = new Scanner(System.in);
+    int age1 = sc.nextInt();
+    String gen1 = sc.next();
+    int age2 = sc.nextInt();
+    String gen2 = sc.next();
+
+    if((age1>=19)||(age2>=19)){
+        if((gen1.equals("M"))||(gen2.equals("M"))){
+            System.out.print(1);
+        }
+    }else{
+        System.out.print(0);
+    }
+    sc.close();
+    }
+}
