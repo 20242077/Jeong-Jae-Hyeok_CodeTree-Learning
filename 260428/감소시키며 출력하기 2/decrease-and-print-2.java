95add06fedf6e5e49b129a5cf32336c32b601234
20242077 <20242077@edu.hanbat.ac.kr>
2026-04-28 16:05:43 +0900

    Title: 감소시키며 출력하기 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260428/감소시키며 출력하기 2/decrease-and-print-2.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+    int n = 26;
+    while(true){
+        if(n<10){break;}
+        System.out.print(n+" ");
+            n -= 1;
+        }
+    }
+}
