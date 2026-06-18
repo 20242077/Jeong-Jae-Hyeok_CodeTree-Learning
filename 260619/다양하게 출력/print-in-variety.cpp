595ac720cfa8e2835768a62b19c0f15f06759d77
20242077 <20242077@edu.hanbat.ac.kr>
2026-06-19 00:55:53 +0900

    [Passed] 다양하게 출력 (2ms, 0MB)

--- 260619/다양하게 출력/print-in-variety.cpp
+++ 260619/다양하게 출력/print-in-variety.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <cstdio>
 using namespace std;
 
 int main() {
     // Please write your code here.
     cout << "Total days in Year\n";
     cout << 365 << endl << "Circumference rate\n";
-    cout << "3.1415926535";
+    printf("%.10f",3.1415926535);
     return 0;
 }
