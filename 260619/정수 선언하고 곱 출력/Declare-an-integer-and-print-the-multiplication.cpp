125390eac7775f2b87352cfbae67c389954cd451
20242077 <20242077@edu.hanbat.ac.kr>
2026-06-19 00:59:02 +0900

    [Passed] 정수 선언하고 곱 출력 (2ms, 0MB)

---
+++ 260619/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 26, b = 5;
+    printf("%d * %d = %d", a,b,a*b);
+    return 0;
+}
