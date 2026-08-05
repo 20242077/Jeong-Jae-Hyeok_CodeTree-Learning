22b94e1ac5849b7661bc39136b57adc553a09f52
20242077 <20242077@edu.hanbat.ac.kr>
2026-08-06 06:15:44 +0900

    [Passed] 특정 수 도달하기 (130ms, 10MB)

---
+++ 260806/특정 수 도달하기/reaching-specific-number.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        int sum = 0;
+        float avg = 0;
+        for(int i = 0; i<10; i++){
+            arr[i] = sc.nextInt();
+            if(arr[i] >= 250){
+                break;
+            }
+            sum += arr[i];
+            avg = (float) sum / (i+1) ;
+        }
+        System.out.printf("%d %.1f",sum,avg);
+    }
+}
