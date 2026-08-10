481f738837af6e9af43f61018b3a6760654d7a2f
20242077 <20242077@edu.hanbat.ac.kr>
2026-08-11 01:56:09 +0900

    [Passed] 문자열 두번 출력하기 (118ms, 10MB)

---
+++ 260811/문자열 두번 출력하기/print-string-twice.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int i = 0;
+        while(i<2){
+            System.out.println(str);
+            i++;
+        }
+    }
+}
