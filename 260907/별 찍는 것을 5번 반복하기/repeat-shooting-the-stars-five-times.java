5638a58dcf46865900f9f5876b9b79f2becb0341
20242077 <20242077@edu.hanbat.ac.kr>
2026-09-07 00:43:30 +0900

    [Passed] 별 찍는 것을 5번 반복하기 (93ms, 9MB)

---
+++ 260907/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void printStars(){
+        for(int i=0;i<5;i++){
+            for(int j=0;j<10;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+    public static void main(String[] args) {
+        // Please write your code here.
+        printStars();
+    }
+}
