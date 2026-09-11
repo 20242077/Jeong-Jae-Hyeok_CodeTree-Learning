ed4f3dd739d6f27902edc8daad87cbc22d3e7001
20242077 <20242077@edu.hanbat.ac.kr>
2026-09-11 21:50:12 +0900

    [Passed] 짝수이면서 합이 5의 배수인 수 (129ms, 10MB)

---
+++ 260911/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void printFunc(int n){
+        if(n % 2==0){
+            if(((n%10)+(n/10))%5==0){
+                System.out.print("Yes");
+            }
+            else{
+                System.out.print("No");
+            }
+        }
+        else{
+            System.out.print("No");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+        printFunc(n);
+    }
+}
