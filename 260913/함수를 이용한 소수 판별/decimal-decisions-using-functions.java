cb37e42684ab56a5d64d9ebee99c6d34ffb0ec32
20242077 <20242077@edu.hanbat.ac.kr>
2026-09-13 03:15:37 +0900

    [Passed] 함수를 이용한 소수 판별 (126ms, 10MB)

---
+++ 260913/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static boolean isPrime(int n){
+        if(n<2){
+            return false;
+        }
+        for(int i=2; i<n; i++){
+            if(n%i==0){
+                return false;
+            }
+        }return true;
+    }
+
+    public static int sumOfPrime(int a,int b){
+        int sum = 0;
+        for(int i=a; i<=b; i++){
+            if(isPrime(i)){
+                sum+=i;
+            }
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        // Please write your code here.
+        System.out.print(sumOfPrime(a,b));
+    }
+}
